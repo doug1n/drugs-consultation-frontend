41259bb32dbeafc5cd9d0073f21779b873ac862d
Douglas Gomes <douglasvieira17@gmail.com>
2021-06-23 17:26:46 -0300

    Created content component

---
+++ src/components/Content/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { Route } from 'react-router-native';
+
+import HomeScreen from '../../screens/Home';
+import HistoryScreen from '../../screens/History';
+import FavoritesScreen from '../../screens/Favorites';
+
+const Container = styled.View`
+  background-color: #fff;
+  flex: 1;
+`;
+
+export default function Content() {
+  return (
+    <Container>
+      <Route exact path="/" component={HomeScreen} />
+      <Route path="/history" component={HistoryScreen} />
+      <Route path="/favorites" component={FavoritesScreen} />
+    </Container>
+  );
+}
